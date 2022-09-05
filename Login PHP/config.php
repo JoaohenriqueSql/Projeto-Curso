bfe39a539f01f492b7ea74f9c22a0ed4d742404f
João Henrique <joao.mariano.henrique@gmail.com>
2022-09-05 00:23:06 -0300

    load in my home

--- Login PHP/config.php
+++ Login PHP/config.php
@@ -4,7 +4,7 @@ servidor com configuração padrão (usuário 'root' sem senha) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'test');
+define('DB_NAME', 'seguranca-dados');
  
 /* Tentativa de conexão com o banco de dados MySQL */
 try{
