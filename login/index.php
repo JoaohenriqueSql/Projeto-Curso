877680dcbc17b24979e16b77309f08c92656500b
mariano.j <izabelcelia39@gmail.com>
2022-09-22 17:24:25 -0300

    style in page initial

--- login/index.php
+++ login/index.php
@@ -10,7 +10,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Segurança da Informação</title>
-  <link rel="stylesheet" href="CSS/style.css">
+  <link rel="stylesheet" href="CSS/estilo-site.css">
   <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </head>
@@ -2574,7 +2574,7 @@
 	</div>
 </div>
 
-<img src="img/onda.svg">
+<img src="img/wave.svg">
 
 <div class="especificacao">
 	<br>
@@ -2608,17 +2608,123 @@
 </div>
 
 
-<div class="container-password">
-	<h1 class="titulo">Gerador de senhas aleatórias</h1>
-	<br>
-	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis reiciendis cumque reprehenderit magnam. Eligendi qui, doloremque sunt quidem eaque provident harum earum quam. Voluptates fuga necessitatibus dolore? Assumenda, doloribus fuga?</p>
-	
-	<div class="text-box">
-      <a href="#" class="btn btn-white btn-animate">click me</a>
-	</div>
+<div class="contentferr">
 
-	<img class="image-senha" src="img/gerador.png">
+	<div class="container-password">
+		<h1 class="titulo">Gerador de senhas aleatórias</h1>
+		<br>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis reiciendis cumque reprehenderit magnam. Eligendi qui, doloremque sunt quidem eaque provident harum earum quam. Voluptates fuga necessitatibus dolore? Assumenda, doloribus fuga?</p>
+		
+		<div class="text-box">
+		<a href="#" class="btn btn-white btn-animate">click me</a>
+		</div>
+
+		<img class="image-senha" src="img/chave-inteligente.png">
+	</div>
 </div>
+
+
+
+
+
+<div class="cont">
+
+<div class="timeline">
+	<ul>
+		<li>
+			<div class="content">
+				<h3>Whats is Lorem Ipsum?</h3>
+				<br>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem aliquam officiis consequatur cumque quod tempora at assumenda sint! Ex impedit error facere amet repellat asperiores numquam dolores, inventore repudiandae magnam.</p>
+			<div>
+			<br>
+			<div class="time">
+				<h4>Junuary 2018</h4>
+			</div>
+		</li>
+		<li>
+			<div class="content">
+				<h3>Whats is Lorem Ipsum?</h3>
+				<br>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem aliquam officiis consequatur cumque quod tempora at assumenda sint! Ex impedit error facere amet repellat asperiores numquam dolores, inventore repudiandae magnam.</p>
+			<div>
+			<br>
+			<div class="time">
+				<h4>Junuary 2018</h4>
+			</div>
+		</li>
+		<li>
+			<div class="content">
+				<h3>Whats is Lorem Ipsum?</h3>
+				<br>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem aliquam officiis consequatur cumque quod tempora at assumenda sint! Ex impedit error facere amet repellat asperiores numquam dolores, inventore repudiandae magnam.</p>
+			<div>
+			<br>
+			<div class="time">
+				<h4>Junuary 2018</h4>
+			</div>
+		</li>
+		<li>
+			<div class="content">
+				<h3>Whats is Lorem Ipsum?</h3>
+				<br>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem aliquam officiis consequatur cumque quod tempora at assumenda sint! Ex impedit error facere amet repellat asperiores numquam dolores, inventore repudiandae magnam.</p>
+			<div>
+			<br>
+			<div class="time">
+				<h4>Junuary 2018</h4>
+			</div>
+		</li>
+		<li>
+			<div class="content">
+				<h3>Whats is Lorem Ipsum?</h3>
+				<br>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem aliquam officiis consequatur cumque quod tempora at assumenda sint! Ex impedit error facere amet repellat asperiores numquam dolores, inventore repudiandae magnam.</p>
+			<div>
+			<br>
+			<div class="time">
+				<h4>Junuary 2018</h4>
+			</div>
+		</li>
+		<li>
+			<div class="content">
+				<h3>Whats is Lorem Ipsum?</h3>
+				<br>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem aliquam officiis consequatur cumque quod tempora at assumenda sint! Ex impedit error facere amet repellat asperiores numquam dolores, inventore repudiandae magnam.</p>
+			<div>
+			<br>
+			<div class="time">
+				<h4>Junuary 2018</h4>
+			</div>
+		</li>
+		<li>
+			<div class="content">
+				<h3>Whats is Lorem Ipsum?</h3>
+				<br>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem aliquam officiis consequatur cumque quod tempora at assumenda sint! Ex impedit error facere amet repellat asperiores numquam dolores, inventore repudiandae magnam.</p>
+			<div>
+			<br>
+			<div class="time">
+				<h4>Junuary 2018</h4>
+			</div>
+		</li>
+		<li>
+			<div class="content">
+				<h3>Whats is Lorem Ipsum?</h3>
+				<br>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem aliquam officiis consequatur cumque quod tempora at assumenda sint! Ex impedit error facere amet repellat asperiores numquam dolores, inventore repudiandae magnam.</p>
+			<div>
+			<br>
+			<div class="time">
+				<h4>Junuary 2018</h4>
+			</div>
+		</li>
+		<div style="clear: both;"></div>		
+	</ul>
+</div>
+
+</div>
+
 
 <script src="JS/script.js"></script>
 </body>
